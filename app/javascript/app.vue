4a1311aa1bd3d5d5705b6aeb049003e330d458c4
runteq-profile <octpsubaru@icloud.com>
2022-03-12 13:52:52 +0900

    メイン画面を作成 #1

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-app id="app">
-    <v-main>
-      <p>{{ message }}</p>
-    </v-main>
-  </v-app>
+<div>
+  <p>{{message}}</p>
+</div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!",
-    };
-  },
-};
+  export default {
+    data() {
+      return {
+        message: "Hello Vue!",
+      }
+    }
+  }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
 </style>
