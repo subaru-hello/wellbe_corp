af774cabd81b70d8a32345b772771373e60f9305
runteq-profile <octpsubaru@icloud.com>
2022-03-15 21:57:28 +0900

    CompositionAPIを試した#7 list rendering,setup(),data(),axios,ref

--- app/javascript/pages/members/MemberIndex.vue
+++ app/javascript/pages/members/MemberIndex.vue
@@ -1,8 +1,53 @@
 <template>
   <p>メンバー情報です</p>
+  <div v-for="(user, index) in users" :key="index" :ref="user">
+    <p>{{ user.nickname }}</p>
+    <p>{{ user.email }}</p>
+  </div>
+  <p>count is {{ count }}</p>
+  <p>plusOne is {{ plusOne }}</p>
+  <button @click="increment">count++</button>
 </template>
 <script>
+import { onMounted, ref, computed, watch } from 'vue'
+const axios = require('axios').default
 export default {
-  setup() {},
+  setup() {
+    // reactive state
+    const users = ref('')
+    const users2 = ref('')
+    const count = ref(0)
+    // computed state
+    const plusOne = computed(() => count.value + 1)
+    // method
+    const increment = () => {
+      count.value++
+    }
+    // watch
+    watch(
+      () => count.value * 2,
+      (val) => {
+        console.log(`count * 2 is ${val}`)
+      }
+    )
+    onMounted(() => {
+      axios
+        .get('api/v1/crues')
+        .then((response) => (users.value = response.data))
+        .catch((error) => console.log(error))
+
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then((response) => (users2.value = response.data))
+        .catch((error) => console.log(error))
+    })
+    return {
+      users,
+      users2,
+      count,
+      plusOne,
+      increment,
+    }
+  },
 }
 </script>
