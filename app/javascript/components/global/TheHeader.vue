42a17df4b8177f3492f2e7bf792fd55b2544e79e
runteq-profile <octpsubaru@icloud.com>
2022-03-21 10:22:25 +0900

    daisyuiを導入 #14

--- app/javascript/components/global/TheHeader.vue
+++ app/javascript/components/global/TheHeader.vue
@@ -1,45 +1,44 @@
 <template>
-  <nav
-    class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"
-  >
-    <div class="container flex flex-wrap justify-between items-center mx-auto">
-      <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-        <ul
-          class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium"
-        >
-          <li>
-            <router-link
+<div class="navbar sticky bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">WelBee</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal p-0">
+      <li> <router-link
               to="/members"
-              class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              class="transition ease-in duration-400 "
               aria-current="page"
               >部員</router-link
-            >
-          </li>
-          <li>
+            ></li>
+      <li tabindex="0">
             <router-link
               to="competitions"
-              class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >競技日程></router-link
+              class="transition ease-in duration-700 "
+              >競技日程</router-link
             >
-          </li>
-          <li>
+        <ul class="p-2 bg-base-100">
+          <li><a>全国</a></li>
+          <li><a>神奈川</a></li>
+        </ul>
+      </li>
+       <li>
             <router-link
               to="records"
-              class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="transition ease-in duration-700 "
               >記録室</router-link
             >
           </li>
-          <li>
+            <li>
             <router-link
               to="/sponsors"
-              class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="transition ease-in duration-700 "
               >スポンサー</router-link
             >
           </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
